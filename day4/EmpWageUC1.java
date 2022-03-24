b8496d8bef9270de7ba96367b3f06f483e8430a2
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-03-24 17:00:41 +0530

    first commit

---
+++ day4/EmpWageUC1.java
@@ -0,0 +1,25 @@
+package day4;
+
+public class EmpWageUC1 {
+
+	public static void main(String[] args) {
+		int fulltime=1;
+		 
+		 double empcheck=Math.floor(Math.random()*10)%2;
+		 if(empcheck == fulltime)
+		 {
+			 System.out.println("Employee is present");
+			
+		 }
+		 else {
+			 System.out.println("Employee is absent");
+		 }
+
+		}
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
+
